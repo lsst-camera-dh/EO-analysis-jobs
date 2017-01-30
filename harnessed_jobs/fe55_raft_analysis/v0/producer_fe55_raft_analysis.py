0d138debbe06d63783fbf57514461c6b5e5fd198
Jim Chiang <jchiang@slac.stanford.edu>
2017-01-30 12:48:10 -0800

    create plots for test report in fe55 analysis producer script

--- harnessed_jobs/fe55_raft_analysis/v0/producer_fe55_raft_analysis.py
+++ harnessed_jobs/fe55_raft_analysis/v0/producer_fe55_raft_analysis.py
@@ -4,6 +4,7 @@ Producer script for raft-level Fe55 analysis.
 """
 from __future__ import print_function
 import os
+import glob
 # This is needed so that pyplot can write to .matplotlib
 os.environ['MPLCONFIGDIR'] = os.curdir
 import matplotlib
@@ -25,12 +26,12 @@ for sensor_id in raft.sensor_names:
                                            jobname=siteUtils.getProcessName('fe55_raft_acq'),
                                            description='Bias files:')
     nf = len(bias_files)
-    outfile = '%(sensor_id)s_mean_bias_%(nf)i.fits' % locals()
-    imutils.fits_mean_file(bias_files, outfile)
+    mean_bias_file = '%(sensor_id)s_mean_bias_%(nf)i.fits' % locals()
+    imutils.fits_mean_file(bias_files, mean_bias_file)
 
     #
     # Create a png zoom of the upper right corner of segment 1 for an Fe55
-    # exposure for inclusion in the test report
+    # exposure for inclusion in the test report.
     #
     print("processing fe55_zoom:", fe55_files[0])
     sensorTest.fe55_zoom(fe55_files[0], size=250, amp=1)
@@ -65,3 +66,36 @@ for sensor_id in raft.sensor_names:
     task = sensorTest.Fe55Task()
     task.config.temp_set_point = -100.
     task.run(sensor_id, fe55_files, (rolloff_mask_file,), accuracy_req=0.01)
+
+    # Fe55 gain and psf analysis results plots for the test report.
+    results_file = '%s_eotest_results.fits' % sensor_id
+    fe55_file = glob.glob('%s_psf_results*.fits' % sensor_id)[0]
+    plots = sensorTest.EOTestPlots(sensor_id, results_file=results_file)
+
+    try:
+        png_file = '%s_fe55_dists.png' % sensor_id
+        plots.fe55_dists(fe55_file=fe55_file)
+        plt.savefig(png_file)
+    except Exception as eobj:
+        print("Exception raised while creating %s:" % png_file)
+        print(eobj)
+
+    try:
+        png_file = '%s_psf_dists.png' % sensor_id
+        plots.psf_dists(fe55_file=fe55_file)
+        plt.savefig(png_file)
+    except Exception as eobj:
+        print("Exception raised while creating %s:" % png_file)
+        print(eobj)
+
+    try:
+        png_file = '%s_gains.png' % sensor_id
+        plots.gains()
+        plt.savefig(png_file)
+    except Exception as eobj:
+        print("Exception raised while creating %s:" % png_file)
+        print(eobj)
+
+    sensorTest.plot_flat(mean_bias_file,
+                         title='%s, mean bias frame' % sensor_id)
+    plt.savefig('%s_mean_bias.png' % sensor_id)
