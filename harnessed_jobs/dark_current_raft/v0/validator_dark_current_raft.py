915c1c1a7ed57bef392b70dfbccab34ae11bc4cd
Jim Chiang <jchiang@slac.stanford.edu>
2017-04-17 15:42:42 -0700

    persist png files

--- harnessed_jobs/dark_current_raft/v0/validator_dark_current_raft.py
+++ harnessed_jobs/dark_current_raft/v0/validator_dark_current_raft.py
@@ -12,6 +12,7 @@ raft = camera_components.Raft.create_from_etrav(raft_id)
 
 results = []
 for slot, sensor_id in raft.items():
+    ccd_vendor = sensor_id.split('-')[0].upper()
     results_file = '%s_eotest_results.fits' % sensor_id
     data = sensorTest.EOTestResults(results_file)
 
@@ -22,6 +23,13 @@ for slot, sensor_id in raft.items():
                                           amp=amp, dark_current_95CL=dc95,
                                           slot=slot,
                                           sensor_id=sensor_id))
+
+    # Persist the png files.
+    metadata = dict(CCD_MANU=ccd_vendor, LSST_NUM=sensor_id,
+                    TESTTYPE='DARK', TEST_CATEGORY='EO')
+    results.extend(siteUtils.persist_png_files('%s*.png' % sensor_id,
+                                               sensor_id, folder=slot,
+                                               metadata=metadata))
 
 results.extend(siteUtils.jobInfo())
 
