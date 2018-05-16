49198f11cd13141df8896aedc4d61ed99a823bb6
Jim Chiang <jchiang@slac.stanford.edu>
2018-05-15 22:42:50 -0700

    add raft-level correlation plot to producer script

--- harnessed_jobs/read_noise_raft/v0/producer_read_noise_raft.py
+++ harnessed_jobs/read_noise_raft/v0/producer_read_noise_raft.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import lsst.eotest.sensor as sensorTest
 import siteUtils
 import eotestUtils
-from correlated_noise import correlated_noise
+from correlated_noise import correlated_noise, raft_level_oscan_correlations
 from multiprocessor_execution import sensor_analyses
 
 
@@ -35,5 +35,27 @@ def run_read_noise_task(sensor_id):
     plt.figure(corr_fig.number)
     plt.savefig('%s_correlated_noise.png' % file_prefix)
 
+
+def get_bias_files(raft_id=None):
+    """Get the bias files from the Fe55 acquisition."""
+    if raft_id is None:
+        raft_id = os.environ['LCATR_UNIT_ID']
+    raft = camera_components.Raft.create_from_etrav(raft_id)
+    bias_files = dict()
+    for slot, sensor_id in raft.items():
+        bias_files[slot] \
+            = siteUtils.dependency_glob('S*/%s_fe55_bias_*.fits' % sensor_id,
+                                        jobname=siteUtils.getProcessName('fe55_raft_acq'),
+                                        description='Bias files for noise correlations:')[0]
+    return bias_files
+
 if __name__ == '__main__':
     sensor_analyses(run_read_noise_task)
+
+    raft_id = os.environ['LCATR_UNIT_ID']
+    run = siteUtils.getRunNumber()
+    bias_files = get_bias_files(raft_id)
+    title = 'Overscan correlations, {}, Run {}'.format(raft_id, run)
+    plt.rcParams['figure.figsize'] = (8, 8)
+    fig = raft_level_oscan_correlations(bias_files, title=title)
+    plt.savefig('{}_{}_overscan_correlations.png'.format(raft_id, run))
