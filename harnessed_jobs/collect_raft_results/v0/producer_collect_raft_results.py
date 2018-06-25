f0b2325029789566807ff1f2790aafd5c0ee2d94
Jim Chiang <jchiang@slac.stanford.edu>
2018-06-25 15:41:30 -0700

    add tearing detection analysis to collect_raft_results job; fix dark current contribution to total noise (see LSSTTD-1279)

--- harnessed_jobs/collect_raft_results/v0/producer_collect_raft_results.py
+++ harnessed_jobs/collect_raft_results/v0/producer_collect_raft_results.py
@@ -5,6 +5,7 @@ an eotest_report.fits file.  Also create raft-level mosaics.
 """
 from __future__ import print_function
 import os
+import pickle
 from collections import OrderedDict
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,6 +15,7 @@ from lcatr.harness.helpers import dependency_glob
 import eotestUtils
 import siteUtils
 import camera_components
+from tearing_detection import tearing_detection
 
 def slot_dependency_glob(pattern, jobname):
     "Return an OrderedDict of files with the desired pattern, keyed by slot."
@@ -30,10 +32,11 @@ total_num, rolloff_mask = sensorTest.pixel_counts(bias_files.values()[0])
 
 # Exposure time (in seconds) for 95th percentile dark current shot
 # noise calculation.
-exptime = 16.
+exptime = 15.
 
 raft_id = siteUtils.getUnitId()
 raft = camera_components.Raft.create_from_etrav(raft_id)
+sensor_ids = {slot: sensor_id for slot, sensor_id in raft.items()}
 summary_files = dependency_glob('summary.lims')
 results_files = dict()
 for slot, sensor_id in raft.items():
@@ -49,7 +52,7 @@ for slot, sensor_id in raft.items():
     # to read noise to produce updated total noise.
     shot_noise = repackager.eotest_results['DARK_CURRENT_95']*exptime
     total_noise = np.sqrt(repackager.eotest_results['READ_NOISE']**2
-                          + shot_noise**2)
+                          + shot_noise)
     for i, amp in enumerate(repackager.eotest_results['AMP']):
         repackager.eotest_results.add_seg_result(amp, 'DC95_SHOT_NOISE',
                                                  np.float(shot_noise[i]))
@@ -67,6 +70,54 @@ for slot, res_file in results_files.items():
 
 title = '%s, %s' % (raft_id, run_number)
 file_prefix = '%s_%s' % (raft_id, run_number)
+
+
+# Run the tearing analysis on the datasets from the acquisition jobs
+# that produce flats:
+# * flat_pair_raft_acq
+# * qe_raft_acq
+# * sflat_raft_acq (These should be separated into low and high flux samples.)
+def get_tearing_stats(pattern, job_name, subset='N/A'):
+    """
+    Collect the tearing stats for each sensor in the raft.
+
+    Parameters
+    ----------
+    pattern: str
+        The file pattern to use in the dependency glob for finding the flats.
+    job_name: str
+        The name of the raft_acq job.
+
+    Returns
+    -------
+    list: list of tuples.  Each tuple contains
+        (job_name, subset, slot, sensor_id, #files with tearing).
+        The subset is "N/A", or for the superflat data, either "low" or "high".
+    """
+    tearing_stats = []
+    flats = slot_dependency_glob(pattern, job_name)
+    for slot, fitsfiles in flats.items():
+        files_with_tearing, _ = tearing_detection(fitsfiles)
+        tearing_stats.append((job_name, subset, slot, sensor_ids[slot],
+                              len(files_with_tearing)))
+    return tearing_stats
+
+tearing_stats = []
+tearing_stats.extend(
+    get_tearing_stats('*flat*flat*.fits', 'flat_pair_raft_acq'))
+tearing_stats.extend(
+    get_tearing_stats('*lambda_flat*.fits', 'qe_raft_acq'))
+tearing_stats.extend(
+    get_tearing_stats('*sflat*flat_L*.fits', 'sflat_raft_acq', 'low_flux'))
+tearing_stats.extend(
+    get_tearing_stats('*sflat*flat_H*.fits', 'sflat_raft_acq', 'high_flux'))
+
+# Pickle these data for the validator script to pick up.
+with open('%s_tearing_stats.pkl' % file_prefix, 'wb') as output:
+    pickle.dump(tearing_stats, output)
+
+
+# Make various plots for the web-based report.
 
 # Raft-level mosaics of median darks, bias, superflats high and low.
 dark_mosaic = raftTest.RaftMosaic(slot_dependency_glob('*median_dark_bp.fits',
--- harnessed_jobs/collect_raft_results/v0/validator_collect_raft_results.py
+++ harnessed_jobs/collect_raft_results/v0/validator_collect_raft_results.py
@@ -3,6 +3,7 @@
 Validator script for collect_raft_results job.
 """
 from __future__ import print_function
+import pickle
 import lcatr.schema
 import siteUtils
 import eotestUtils
@@ -36,6 +37,17 @@ for slot, sensor_id in raft.items():
 metadata = dict(CCD_MANU=ccd_vendor, TEST_CATEGORY='EO')
 results.extend(siteUtils.persist_png_files('%s*.png' % raft_id,
                                            raft_id, metadata=metadata))
+
+# Persist the tearing results.
+file_prefix = '%s_%s' % (raft_id, run_number)
+with open('%s_tearing_stats.pkl' % file_prefix, 'rb') as input_:
+    tearing_stats = pickle.load(input_)
+schema = lcatr.schema.get('tearing_detection')
+for values in tearing_stats:
+    stats = dict(kv_pair for kv_pair in
+                 zip(('job_name', 'subset', 'slot', 'sensor_id', 'detections'),
+                     values))
+    results.append(lcatr.schema.valid(schema, **stats))
 
 results.extend(siteUtils.jobInfo())
 lcatr.schema.write_file(results)
