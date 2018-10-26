b59aea2897ed1e04d5cead6db43edc4407e1e757
James Chiang <jchiang@slac.stanford.edu>
2018-10-25 17:19:39 -0700

    add BOT versions of fe55, read noise, and bright defects analysis jobs

--- harnessed_jobs/BOT_EO_analysis/v0/producer_BOT_EO_analysis.py
+++ harnessed_jobs/BOT_EO_analysis/v0/producer_BOT_EO_analysis.py
@@ -4,11 +4,96 @@ Producer script for raft-level Fe55 analysis.
 """
 from __future__ import print_function
 import glob
+import matplotlib.pyplot as plt
 import lsst.eotest.image_utils as imutils
 import lsst.eotest.sensor as sensorTest
 import siteUtils
+from correlated_noise import correlated_noise, raft_level_oscan_correlations
 from camera_components import camera_info
 from multiprocessor_execution import run_sensor_analysis_pool
+
+
+def get_amplifier_gains(eotest_results_file):
+    """Extarct the gains for each amp in an eotest_results file."""
+    data = sensorTest.EOTestResults(eotest_results_file)
+    amps = data['AMP']
+    gains = data['GAIN']
+    return {amp: gain for amp, gain in zip(amps, gains)}
+
+
+def bright_defects_task(det_name):
+    "Single sensor execution of the bright pixels task."
+    run = siteUtils.getRunNumber()
+    file_prefix = '%s_%s' % (run, det_name)
+    dark_files \
+        = siteUtils.dependency_glob('dark_dark_*/*_{}.fits'.format(det_name))
+    if not dark_files:
+        print("Needed data files missing for run {}, detector {}."
+              .format(run, det_name))
+        return
+
+    eotest_results_file = '{}_eotest_results.fits'.format(file_prefix)
+    gains = get_amplifier_gains(eotest_results_file)
+    mask_files = sorted(glob.glob('{}_*mask.fits'.format(file_prefix)))
+
+    task = sensorTest.BrightPixelsTask()
+    task.config.temp_set_point = -100.
+    task.run(file_prefix, dark_files, mask_files, gains)
+
+    siteUtils.make_png_file(sensorTest.plot_flat,
+                            '%s_medianed_dark.png' % file_prefix,
+                            '%s_median_dark_bp.fits' % file_prefix,
+                            title='%s, medianed dark for bright defects analysis' % file_prefix,
+                            annotation='e-/pixel, gain-corrected, bias-subtracted')
+
+
+def read_noise_task(det_name):
+    """Run the single sesnor read noise task."""
+    run = siteUtils.getRunNumber()
+    file_prefix = '%s_%s' % (run, det_name)
+    bias_files \
+        = siteUtils.dependency_glob('fe55_fe55_*/*_{}.fits'.format(det_name))
+    if not bias_files:
+        print("Needed data files are missing for run {}, detector {}."
+              .format(run, det_name))
+        return
+    eotest_results_file = '{}_eotest_results.fits'.format(file_prefix)
+    gains = get_amplifier_gains(eotest_results_file)
+
+    system_noise = None
+
+    mask_files = sorted(glob.glob('{}_*mask.fits'.format(file_prefix)))
+
+    task = sensorTest.ReadNoiseTask()
+    task.config.temp_set_point = -100.
+    task.run(file_prefix, bias_files, gains, system_noise=system_noise,
+             mask_files=mask_files, use_overscan=True)
+
+    # Compute amp-amp correlated noise.
+    _, corr_fig, _ = correlated_noise(bias_files, target=0,
+                                      make_plots=True, title=file_prefix)
+    plt.figure(corr_fig.number)
+    plt.savefig('%s_correlated_noise.png' % file_prefix)
+
+
+def raft_noise_correlations(raft_name):
+    """This is the raft-level noise-correlation analysis."""
+    run = siteUtils.getRunNumber()
+    file_prefix = '{}_{}'.format(run, raft_name)
+    bias_files = dict()
+    slot_names = camera_info.get_slot_names()
+    for slot_name in slot_names:
+        det_name = '{}_{}'.format(raft_name, slot_name)
+        my_files \
+            = siteUtils.dependency_glob('fe55_bias_*/*_{}.fits'.format(det_name))
+        if not my_files:
+            print("Missing bias files for raft {}. Skipping.".format(raft_name))
+            return
+        bias_files[slot_name] = my_files[0]
+    title = 'Overscan correlations, Run {}, {}'.format(raft_name, run)
+    raft_level_oscan_correlations(bias_files, title=title)
+    plt.savefig('{}_overscan_correlations.png'.format(file_prefix))
+
 
 def fe55_task(det_name):
     "Single sensor execution of the Fe55 analysis task."
@@ -17,6 +102,11 @@ def fe55_task(det_name):
     bias_files \
         = siteUtils.dependency_glob('fe55_bias_*/*_{}.fits'.format(det_name))
     file_prefix = '{}_{}'.format(siteUtils.getRunNumber(), det_name)
+
+    if not fe55_files or not bias_files:
+        print("Needed data files missing for run {}, detector {}."
+              .format(siteUtils.getRunNumber(), det_name))
+        return
 
     mean_bias_file = '{}_mean_bias.fits'.format(file_prefix)
     imutils.fits_mean_file(bias_files, mean_bias_file)
@@ -66,4 +156,10 @@ def fe55_task(det_name):
 
 if __name__ == '__main__':
     det_names = camera_info.get_det_names()
-    run_sensor_analysis_pool(fe55_task, det_names, processes=1)
+    raft_names = camera_info.get_raft_names()
+
+    run_sensor_analysis_pool(fe55_task, det_names, processes=None)
+    run_sensor_analysis_pool(read_noise_task, det_names, processes=None)
+    for raft_name in raft_names:
+        raft_noise_correlations(raft_name)
+    run_sensor_analysis_pool(bright_defects_task, det_names, processes=None)
--- harnessed_jobs/BOT_EO_analysis/v0/validator_BOT_EO_analysis.py
+++ harnessed_jobs/BOT_EO_analysis/v0/validator_BOT_EO_analysis.py
@@ -1,66 +1,155 @@
 #!/usr/bin/env python
 """
-Validator script for raft-level Fe55 analysis.
+Validator script for BOT-level Fe55 analysis.
 """
 from __future__ import print_function
+import os
 import glob
 import numpy as np
 import lcatr.schema
 import siteUtils
 import eotestUtils
 import lsst.eotest.sensor as sensorTest
-import camera_components
+from camera_components import camera_info
 
-raft_id = siteUtils.getUnitId()
-raft = camera_components.Raft.create_from_etrav(raft_id)
+def validate_fe55(results, det_names):
+    """Validate and persist fe55 gain and psf results."""
+    for det_name in det_names:
+        raft, slot = det_name.split('_')
+        file_prefix = '{}_{}'.format(siteUtils.getRunNumber(), det_name)
 
-results = []
-for slot, sensor_id in raft.items():
-    ccd_vendor = sensor_id.split('-')[0].upper()
-    # The output files from producer script.
-    gain_file = '%(sensor_id)s_eotest_results.fits' % locals()
-    psf_results = glob.glob('%(sensor_id)s_psf_results*.fits' % locals())[0]
-    rolloff_mask = '%(sensor_id)s_rolloff_defects_mask.fits' % locals()
+        # The output files from producer script.
+        gain_file = '%(file_prefix)s_eotest_results.fits' % locals()
+        psf_results_files \
+            = glob.glob('%(file_prefix)s_psf_results*.fits' % locals())
 
-    output_files = gain_file, psf_results, rolloff_mask
+        if not os.path.isfile(gain_file) or not psf_results_files:
+            # Results for this detector are not available so skip it.
+            continue
+        psf_results = psf_results_files[0]
 
-    # Add/update the metadata to the primary HDU of these files.
-    for fitsfile in output_files:
-        eotestUtils.addHeaderData(fitsfile, LSST_NUM=sensor_id, TESTTYPE='FE55',
-                                  DATE=eotestUtils.utc_now_isoformat(),
-                                  CCD_MANU=ccd_vendor)
+        rolloff_mask = '%(file_prefix)s_edge_rolloff_mask.fits' % locals()
 
-    #
-    # Persist the mean bias FITS file.
-    #
-    bias_mean_file = glob.glob('%(sensor_id)s_mean_bias_*.fits' % locals())[0]
-    results.append(siteUtils.make_fileref(bias_mean_file, folder=slot))
+        output_files = gain_file, psf_results, rolloff_mask
 
-    # Persist the png files.
-    metadata = dict(CCD_MANU=ccd_vendor, LSST_NUM=sensor_id,
-                    TESTTYPE='FE55', TEST_CATEGORY='EO')
-    results.extend(siteUtils.persist_png_files('%s*.png' % sensor_id,
-                                               sensor_id, folder=slot,
-                                               metadata=metadata))
+        # Add/update the metadata to the primary HDU of these files.
+        for fitsfile in output_files:
+            eotestUtils.addHeaderData(fitsfile, TESTTYPE='FE55',
+                                      DATE=eotestUtils.utc_now_isoformat())
 
-    data = sensorTest.EOTestResults(gain_file)
-    amps = data['AMP']
-    gain_data = data['GAIN']
-    gain_errors = data['GAIN_ERROR']
-    sigmas = data['PSF_SIGMA']
-    for amp, gain_value, gain_error, sigma in zip(amps, gain_data, gain_errors,
-                                                  sigmas):
-        if not np.isfinite(gain_error):
-            gain_error = -1
-        results.append(lcatr.schema.valid(lcatr.schema.get('fe55_raft_analysis'),
-                                          amp=amp, gain=gain_value,
-                                          gain_error=gain_error,
-                                          psf_sigma=sigma,
-                                          slot=slot,
-                                          sensor_id=sensor_id))
+        # Persist the mean bias FITS file.
+        bias_mean_file \
+            = glob.glob('%(file_prefix)s_mean_bias.fits' % locals())[0]
+        print("make_fileref:", bias_mean_file)
+        results.append(siteUtils.make_fileref(bias_mean_file))
 
-    results.extend([lcatr.schema.fileref.make(x) for x in output_files])
+        # Persist the png files.
+        metadata = dict(TESTTYPE='FE55', TEST_CATEGORY='EO')
+        results.extend(siteUtils.persist_png_files('%s*.png' % file_prefix,
+                                                   det_name, metadata=metadata))
 
-results.extend(siteUtils.jobInfo())
-lcatr.schema.write_file(results)
-lcatr.schema.validate_file()
+        data = sensorTest.EOTestResults(gain_file)
+        amps = data['AMP']
+        gain_data = data['GAIN']
+        gain_errors = data['GAIN_ERROR']
+        sigmas = data['PSF_SIGMA']
+        for amp, gain_value, gain_error, sigma in zip(amps, gain_data,
+                                                      gain_errors, sigmas):
+            if not np.isfinite(gain_error):
+                gain_error = -1
+            results.append(lcatr.schema.valid(
+                lcatr.schema.get('fe55_BOT_analysis'), amp=amp, gain=gain_value,
+                gain_error=gain_error, psf_sigma=sigma, slot=slot, raft=raft))
+
+        results.extend([lcatr.schema.fileref.make(x) for x in output_files])
+    return results
+
+
+def validate_read_noise(results, det_names):
+    """Validate and persist read noise results."""
+    for det_name in det_names:
+        raft, slot = det_name.split('_')
+        file_prefix = '{}_{}'.format(siteUtils.getRunNumber(), det_name)
+
+        read_noise_file = '%s_eotest_results.fits' % file_prefix
+        if not os.path.isfile(read_noise_file):
+            print("read noise results not available for", det_name)
+            continue
+        data = sensorTest.EOTestResults(read_noise_file)
+        amps = data['AMP']
+        read_noise_data = data['READ_NOISE']
+        system_noise_data = data['SYSTEM_NOISE']
+        total_noise_data = data['TOTAL_NOISE']
+        for amp, read_noise, system_noise, total_noise \
+            in zip(amps, read_noise_data, system_noise_data, total_noise_data):
+            results.append(lcatr.schema.valid(
+                lcatr.schema.get('read_noise_BOT'),
+                amp=amp, read_noise=read_noise, system_noise=system_noise,
+                total_noise=total_noise, slot=slot, raft=raft))
+
+        files = glob.glob('%s_read_noise?*.fits' % file_prefix)
+        for fitsfile in files:
+            eotestUtils.addHeaderData(fitsfile, TESTTYPE='FE55',
+                                      DATE=eotestUtils.utc_now_isoformat())
+
+        data_products = [siteUtils.make_fileref(item) for item in files]
+        results.extend(data_products)
+
+        # Persist the png files.
+        metadata = dict(TESTTYPE='FE55', TEST_CATEGORY='EO')
+        results.extend(siteUtils.persist_png_files('%s*.png' % file_prefix,
+                                                   det_name, metadata=metadata))
+
+    # Persist the raft-level overscan correlation plots.
+    metadata = dict(TESTTYPE='FE55', TEST_CATEGORY='EO')
+    results.extend(siteUtils.persist_png_files('%s*.png' % file_prefix,
+                                               raft, metadata=metadata))
+    return results
+
+
+def validate_bright_defects(results, det_names):
+    """Validate and persist bright defects results."""
+    for det_name in det_names:
+        raft, slot = det_name.split('_')
+        file_prefix = '{}_{}'.format(siteUtils.getRunNumber(), det_name)
+        mask_file = '%s_bright_pixel_mask.fits' % file_prefix
+        if not os.path.isfile(mask_file):
+            print("mask file not found for {}. Skipping.".format(det_name))
+            continue
+        eotestUtils.addHeaderData(mask_file, TESTTYPE='DARK',
+                                  DATE=eotestUtils.utc_now_isoformat())
+        results.append(siteUtils.make_fileref(mask_file))
+
+        medianed_dark = '%s_median_dark_bp.fits' % file_prefix
+        eotestUtils.addHeaderData(medianed_dark,
+                                  DATE=eotestUtils.utc_now_isoformat())
+        results.append(siteUtils.make_fileref(medianed_dark))
+
+        eotest_results = '%s_eotest_results.fits' % file_prefix
+        data = sensorTest.EOTestResults(eotest_results)
+        amps = data['AMP']
+        npixels = data['NUM_BRIGHT_PIXELS']
+        ncolumns = data['NUM_BRIGHT_COLUMNS']
+        for amp, npix, ncol in zip(amps, npixels, ncolumns):
+            results.append(lcatr.schema.valid(
+                lcatr.schema.get('bright_defects_BOT'),
+                amp=amp, bright_pixels=npix, bright_columns=ncol,
+                slot=slot, raft=raft))
+        # Persist the png files.
+        metadata = dict(TESTTYPE='DARK', TEST_CATEGORY='EO')
+        results.extend(siteUtils.persist_png_files('%s*.png' % file_prefix,
+                                                   det_name, metadata=metadata))
+    return results
+
+
+if __name__ == '__main__':
+    det_names = camera_info.get_det_names()
+    results = []
+    results.extend(validate_fe55(results, det_names))
+    results.extend(validate_read_noise(results, det_names))
+    results.extend(validate_bright_defects(results, det_names))
+
+    results.extend(siteUtils.jobInfo())
+
+    lcatr.schema.write_file(results)
+    lcatr.schema.validate_file()
