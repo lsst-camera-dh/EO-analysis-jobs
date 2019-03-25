1d945b99c827cb06754f31f1b42c2bf1035f8363
James Chiang <jchiang@slac.stanford.edu>
2019-03-24 22:08:29 -0700

    add dark current job

---
+++ harnessed_jobs/dark_current_BOT/v0/producer_dark_current_BOT.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+"""
+Producer script for BOT dark current analysis.
+"""
+from bot_eo_analyses import run_det_task_analysis
+run_det_task_analysis('dark')
---
+++ harnessed_jobs/dark_current_BOT/v0/validator_dark_current_BOT.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+"""
+Validator script for BOT dark current analysis.
+"""
+from bot_eo_validators import run_validator
+run_validator('dark_current')
