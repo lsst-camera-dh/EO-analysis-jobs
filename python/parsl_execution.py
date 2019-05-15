98f72efc0b1589ff315b24cb419a817ab7db6226
Eric Charles <echarles@gmail.com>
2019-05-15 16:07:33 -0700

    Added parsl_execution and config

---
+++ python/parsl_execution.py
@@ -0,0 +1,107 @@
+"""
+Function to parallelize subcomponent analyses for an assembly such
+as raft or full focal plane.
+"""
+import os
+import multiprocessing
+import siteUtils
+import camera_components
+
+import parsl
+from parsl.app.app import python_app
+
+from .parsl_irs_dc_config import IR2_DC_CONFIG, MAX_PARSL_THREADS
+
+parsl.load(IR2_DC_CONFIG)
+
+
+__all__ = ['parsl_sensor_analyses', 'parsl_device_analysis_pool']
+
+@python_app
+def parsl_wrapper(func, *args, **kwargs):
+    return func(*args, **kwargs)
+
+
+def parsl_device_analysis_pool(task_func, device_names, processes=None):
+    """
+    Use a multiprocessing.Pool to run a device-level analysis task
+    over a collection of device names.  The task_func should be
+    implemented as pickleable function that takes the desired device
+    name as its single argument.
+
+    Parameters
+    ----------
+    task_func: function
+        A pickleable function that takes the detector name string as
+        its argument.
+    device_names: list
+        The list of device names to run in the pool.
+    processes : int, optional
+        The maximum number of processes to have running at once.
+        If None (default), then set to 1 or one less than
+        the number of cores, whichever is larger.
+
+    Notes
+    -----
+    Exceptions from subprocesses will be buffered until all of the
+    subprocesses have finished running.  If any exceptions are thrown
+    and are uncaught, a non-zero exit code will be generated for the
+    overall process. Output to stdout or stderr from the subprocesses
+    will be interleaved.
+
+    Users can override the default or keyword argument values by setting
+    the LCATR_PARALLEL_PROCESSES environment variable.
+    """
+    if processes is None:
+        # Use the maximum number of cores available, reserving one for
+        # the parent process.
+        processes = max(1, MAX_PARSL_THREADS - 1)
+    processes = int(os.environ.get('LCATR_PARALLEL_PROCESSES', processes))
+
+    if processes == 1:
+        # For cases where only one process will be run at a time, it's
+        # faster to run serially instead of using a
+        # multiprocessing.Pool since the pickling that occurs can
+        # cause significant overhead.
+        for device_name in device_names:
+            task_func(device_name)
+    else:
+        outputs = [ parsl_wrapper(task_func, device_name).result() for device_name in device_names ]
+
+
+def parsl_sensor_analyses(run_task_func, raft_id=None, processes=None):
+    """
+    Run a sensor-level analysis task implemented as a pickleable
+    function that takes the desired sensor id as its single argument.
+
+    Parameters
+    ----------
+    run_task_func : function
+        A pickleable function that takes the sensor_id string as
+        its argument.
+    raft_id : str, optional
+        The RTM (or RSA) LSST ID.  If None (default), the LCATR_UNIT_ID
+        is used.
+    processes : int, optional
+        The maximum number of processes to have running at once.
+        If None (default), then set to 1 or one less than
+        the number of cores, whichever is larger.
+    """
+    if raft_id is None:
+        raft_id = siteUtils.getUnitId()
+
+    raft = camera_components.Raft.create_from_etrav(raft_id)
+
+    parsl_device_analysis_pool(run_task_func, raft.sensor_names,
+                               processes=processes)
+
+
+def print_device_name(device_name):
+    sys.stdout.write("%s\n" % device_name)
+
+
+if __name__ == '__main__':
+    import os
+    os.environ['LCATR_LIMS_URL'] = 'Dummy Prod'
+    raft_id = 'LCA-11021_RTM-004-Dev'
+    parsl_sensor_analyses(print_device_name, raft_id)
---
+++ python/parsl_ir2_dc_config.py
@@ -0,0 +1,32 @@
+
+import parsl
+import os
+
+from parsl.app.app import python_app
+from parsl.executors import HighThroughputExecutor
+from parsl.providers import LocalProvider
+from parsl.channels import SSHChannel
+from parsl.config import Config
+
+
+SETUP_SCRIPT = '/home/echarles/lsst_setup.bashrc'
+
+WORKER_NODE_ADDRESSES = ['lsst-dc01', 'lsst-dc02', 'lsst-dc03',
+                         'lsst-dc04', 'lsst-dc05', 'lsst-dc06',
+                         'lsst-dc07', 'lsst-dc08', 'lsst-dc09']
+MOTHER_NODE_ADDRESS = 'lsst-dc10'
+NCORES = 64
+
+PROVIDERS = [LocalProvider(channel=SSHChannel(hostname=hostn,
+                                              no_auth=True),
+                           worker_init='source %s' % SETUP_SCRIPT) for hostn in WORKER_NODE_ADDRESSES]
+
+EXECUTORS = [HighThroughputExecutor(label="Ad-Hoc_%i" % iprov,
+                                    address=MOTHER_NODE_ADDRESS,
+                                    worker_debug=True,
+                                    cores_per_worker=NCORES,
+                                    provider=provider) for iprov, provider in enumerate(PROVIDERS)]
+
+IR2_DC_CONFIG = Config(executors=EXECUTORS, strategy=None)
+
+MAX_PARSL_THREADS = len(WORKER_NODE_ADDRESSES) * NCORES
