3560f281071c86fca4649ae3345b9ef3844de785
James Chiang <jchiang@slac.stanford.edu>
2019-05-23 14:58:18 -0700

    enable any mother node; refactor to use hostnames for executor labels; pass script_dir to SSHChannel

--- python/parsl_ir2_dc_config.py
+++ python/parsl_ir2_dc_config.py
@@ -1,40 +1,61 @@
-
+"""
+parsl configuration for running on IR2 diagnostic cluster.
+"""
 import os
-
+import socket
+import logging
 import parsl
 from parsl.executors import HighThroughputExecutor
 from parsl.providers import LocalProvider
 from parsl.channels import SSHChannel
 from parsl.config import Config
 
+__all__ = ['load_ir2_dc_config', 'MAX_PARSL_THREADS', 'PARSL_LOADED']
 
-SETUP_SCRIPT = '/home/echarles/lsst_setup.bashrc'
 
-WORKER_NODE_ADDRESSES = ['lsst-dc01', 'lsst-dc02', 'lsst-dc03',
-                         'lsst-dc04', 'lsst-dc05', 'lsst-dc06',
-                         'lsst-dc07', 'lsst-dc08', 'lsst-dc09']
+# Suppress the verbose default debug-level logging:
+parsl.set_stream_logger(name='interchange', level=logging.INFO)
 
-MOTHER_NODE_ADDRESS = 'lsst-dc10'
-NCORES = 48
+SETUP_SCRIPT = os.environ.get('LCATR_SETUP_SCRIPT',
+                              os.path.join(os.environ['INST_DIR'], 'setup.sh'))
 
-MAX_PARSL_THREADS = len(WORKER_NODE_ADDRESSES) * NCORES
+WORKER_NODE_ADDRESSES = set(['lsst-dc01', 'lsst-dc02', 'lsst-dc03',
+                             'lsst-dc04', 'lsst-dc05', 'lsst-dc06',
+                             'lsst-dc07', 'lsst-dc08', 'lsst-dc09',
+                             'lsst-dc10'])
+MOTHER_NODE_ADDRESS = socket.gethostname().split('.')[0]
+WORKER_NODE_ADDRESSES.remove(MOTHER_NODE_ADDRESS)
+NCORES = 28
 
 PARSL_LOADED = False
 
+MAX_PARSL_THREADS = len(WORKER_NODE_ADDRESSES) * NCORES
+
+def script_dir(hostname, root_dir='.'):
+    """
+    Compose a directory path for local provider scripts.
+    """
+    return os.path.join(os.path.abspath(root_dir), 'runinfo', hostname)
+
+
 def load_ir2_dc_config():
+    """
+    Load the parsl config for ad-hoc providers.
+    """
+    executors = []
 
-    providers = [LocalProvider(channel=SSHChannel(hostname=hostn,
-                                                  no_auth=True),
-                               worker_init='source %s' % SETUP_SCRIPT) for hostn in WORKER_NODE_ADDRESSES]
-
-    executors = [HighThroughputExecutor(label="Ad-Hoc_%i" % iprov,
-                                        address=MOTHER_NODE_ADDRESS,
-                                        worker_debug=True,
-                                        cores_per_worker=NCORES,
-                                        provider=provider) for iprov, provider in enumerate(providers)]
+    for host in WORKER_NODE_ADDRESSES:
+        channel = SSHChannel(hostname=host, script_dir=script_dir(host))
+        provider = LocalProvider(channel=channel,
+                                 worker_init='source %s' % SETUP_SCRIPT)
+        executors.append(HighThroughputExecutor(label=host,
+                                                address=MOTHER_NODE_ADDRESS,
+                                                worker_debug=False,
+                                                cores_per_worker=NCORES,
+                                                provider=provider))
 
     config = Config(executors=executors, strategy=None)
-    
+
     parsl.load(config)
 
     PARSL_LOADED = True
