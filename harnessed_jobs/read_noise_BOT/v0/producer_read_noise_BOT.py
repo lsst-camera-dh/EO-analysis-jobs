c151b607b79538aba585e8d2592840ce4e40dd61
James Chiang <jchiang@slac.stanford.edu>
2019-07-01 14:33:21 -0700

    ensure files from the same bias frame are used; enable logging in read_noise_BOT functions

--- harnessed_jobs/read_noise_BOT/v0/producer_read_noise_BOT.py
+++ harnessed_jobs/read_noise_BOT/v0/producer_read_noise_BOT.py
@@ -6,9 +6,13 @@ def read_noise_jh_task(det_name):
     """JH version of the single sensor read noise task."""
     import os
     import glob
+    import logging
     import siteUtils
     from bot_eo_analyses import make_file_prefix, glob_pattern,\
         get_amplifier_gains, read_noise_task
+
+    logger = logging.getLogger('read_noise_jh_task')
+    logger.setLevel(logging.INFO)
 
     run = siteUtils.getRunNumber()
     file_prefix = make_file_prefix(run, det_name)
@@ -19,8 +23,8 @@ def read_noise_jh_task(det_name):
         = siteUtils.dependency_glob(glob_pattern('read_noise', det_name),
                                     acq_jobname=acq_jobname)[:nbias]
     if not bias_files:
-        print("read_noise_task: Needed data files are missing for detector",
-              det_name)
+        logger.info("read_noise_task: Needed data files are missing "
+                    "for detector %s", det_name)
         return None
     eotest_results_file = '{}_eotest_results.fits'.format(file_prefix)
     gains = get_amplifier_gains(eotest_results_file)
@@ -34,9 +38,14 @@ def read_noise_jh_task(det_name):
 def raft_jh_noise_correlations(raft_name):
     """JH version of raft-level noise-correlation analysis."""
     import os
+    from collections import defaultdict
+    import logging
     import siteUtils
     from camera_components import camera_info
     from bot_eo_analyses import raft_noise_correlations, glob_pattern
+
+    logger = logging.getLogger('raft_jh_noise_correlations')
+    logger.setLevel(logging.INFO)
 
     run = siteUtils.getRunNumber()
     acq_jobname = siteUtils.getProcessName('BOT_acq')
@@ -46,19 +55,25 @@ def raft_jh_noise_correlations(raft_name):
                                                  '{}_S??'.format(raft_name)),
                                     acq_jobname=acq_jobname)
     if not bias_files:
-        print("raft_noise_correlations: Missing bias files for raft",
-              raft_name)
+        logging.info("raft_noise_correlations: Missing bias files for raft %s",
+                     raft_name)
         return None
-    bias_file_dict = dict()
-    slot_names = camera_info.get_slot_names()
+    bias_frame_dict = defaultdict(dict)
     for item in bias_files:
-        for slot_name in slot_names:
-            if slot_name in bias_file_dict:
-                continue
-            det_name = '{}_{}'.format(raft_name, slot_name)
-            if det_name in os.path.basename(item):
-                bias_file_dict[slot_name] = item
-    return raft_noise_correlations(run, raft_name, bias_file_dict)
+        # The exposure is identified by test type, image type, and
+        # seqno in the name of the folder containing the FITS files.
+        seqno = os.path.dirname(item).split('_')[-1]
+        # slot_name is the last '_'-delimited field before '.fits'.
+        slot_name = item.split('_')[-1].split('.')[0]
+        bias_frame_dict[seqno][slot_name] = item
+    for seqno in bias_frame_dict:
+        if len(bias_frame_dict[seqno]) == 9:
+            bias_frame_files = bias_frame_dict[seqno]
+            break
+    logger.info("bias frame files for raft_noise_correlations:")
+    for key, value in bias_frame_files.items():
+        logger.info("   %s", value)
+    return raft_noise_correlations(run, raft_name, bias_frame_files)
 
 
 if __name__ == '__main__':
