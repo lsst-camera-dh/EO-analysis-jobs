eaa5ded8a602e3a51e44530fae968632d28c63e6
James Chiang <jchiang@slac.stanford.edu>
2019-11-04 14:17:46 -0800

    apply log scale to pixel defects results in focal plane heat maps; apply overall scale factor to colorbar for cti heat maps

--- harnessed_jobs/raft_results_summary_BOT/v0/producer_raft_results_summary_BOT.py
+++ harnessed_jobs/raft_results_summary_BOT/v0/producer_raft_results_summary_BOT.py
@@ -17,24 +17,35 @@ def make_focal_plane_plots():
     #
     fp_configs = {'fe55_BOT_analysis': (('gain', 'psf_sigma'),
                                         ((0.5, 1.5), (3, 5.5)),
-                                        (('e-/ADU', 'micron'))),
-                  'read_noise_BOT': (('read_noise',), ((0, 20),), ('e-',)),
+                                        ('e-/ADU', 'micron'),
+                                        (False, False),
+                                        ('1', '1')),
+                  'read_noise_BOT': (('read_noise',), ((0, 20),), ('e-',),
+                                     (False,), ('1',)),
                   'bright_defects_BOT': (('bright_pixels', 'bright_columns'),
-                                         (None, None), (None, None)),
+                                         (None, None), (None, None),
+                                         (True, True), ('1', '1')),
                   'dark_defects_BOT': (('dark_pixels', 'dark_columns'),
-                                       (None, None), (None, None)),
+                                       (None, None), (None, None),
+                                       (True, True), ('1', '1')),
                   'dark_current_BOT': (('dark_current_95CL',), ((0, 0.3),),
-                                       ('e-/pix/s',)),
-                  'traps_BOT': (('num_traps',), (None,), (None,)),
+                                       ('e-/pix/s',), (False,), ('1',)),
+                  'traps_BOT': (('num_traps',), (None,), (None,), (False,),
+                                ('1',)),
                   'cte_BOT': (('cti_high_serial', 'cti_high_parallel',
                                'cti_low_serial', 'cti_low_parallel'),
                               ((0, 1e-5), (0, 1e-5), (0, 1e-5), (0, 1e-5)),
-                              (None, None, None, None)),
-                  'flat_pairs_BOT': (('max_frac_dev',), ((0, 0.05),), (None,)),
+                              (None, None, None, None),
+                              (False, False, False, False),
+                              ('1e-6', '1e-6', '1e-6', '1e-6')),
+                  'flat_pairs_BOT': (('max_frac_dev', 'full_well'),
+                                     ((0, 0.05), None), (None, None),
+                                     (False, False), ('1', '1')),
                   'ptc_BOT': (('ptc_gain', 'ptc_a00'), ((0.5, 1.5), (0, 5e-6)),
-                              ('e-/ADU', None)),
+                              ('e-/ADU', None), (False, False), ('1', '1')),
                   'bright_fatter_BOT': (('bf_xcorr', 'bf_ycorr'), (None, None),
-                                        (None, None))}
+                                        (None, None), (False, False),
+                                        ('1', '1'))}
 
     camera = camera_info.camera_object
 
@@ -42,7 +53,8 @@ def make_focal_plane_plots():
     unit_id = siteUtils.getUnitId()
     et_results = siteUtils.ETResults(run)
     for schema_name, configs in fp_configs.items():
-        for column, z_range, units in zip(*configs):
+        for column, z_range, units, use_log10, \
+            inv_scale_factor in zip(*configs):
             fig = plt.figure(figsize=(12, 10))
             ax = fig.add_subplot(1, 1, 1)
             try:
@@ -51,7 +63,9 @@ def make_focal_plane_plots():
                 print("No focal plane results for {}: {} in eTraveler."
                       .format(schema_name, column))
             else:
-                plot_focal_plane(ax, amp_data, camera=camera, z_range=z_range)
+                plot_focal_plane(ax, amp_data, camera=camera, z_range=z_range,
+                                 use_log10=use_log10,
+                                 inv_scale_factor=inv_scale_factor)
                 if units is not None:
                     plt.title('Run {}, {} ({})'.format(run, column, units))
                 else:
