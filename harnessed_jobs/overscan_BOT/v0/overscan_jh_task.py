63be07bd8743eb3742a89f0168754e6bfedadbcc
James Chiang <jchiang@slac.stanford.edu>
2020-03-09 09:28:15 -0700

    add overscan job to BOT analyses

---
+++ harnessed_jobs/overscan_BOT/v0/overscan_jh_task.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env ipython
+"""
+Producer script for BOT overscan analysis.
+"""
+def overscan_jh_task(det_name):
+    """JH version of single sensor execution of the overscan task."""
+    import siteUtils
+    from bot_eo_analyses import make_file_prefix, glob_pattern,\
+        get_amplifier_gains, bias_filename, overscan_task
+
+    run = siteUtils.getRunNumber()
+    file_prefix = make_file_prefix(run, det_name)
+    acq_jobname = siteUtils.getProcessName('BOT_acq')
+
+    flat_files = siteUtils.dependency_glob(glob_pattern('ptc', det_name),
+                                           acq_jobname=acq_jobname)
+    if not flat_files:
+        print("ptc_task: Flat pairs files not found for detector", det_name)
+        return None
+
+    eotest_results_file = '{}_eotest_results.fits'.format(file_prefix)
+    gains = get_amplifier_gains(eotest_results_file)
+    bias_frame = bias_filename(run, det_name)
+
+    return overscan_task(run, det_name, flat_files, gains,
+                         bias_frame=bias_frame)
+
+if __name__ == '__main__':
+    import sys
+    det_name = sys.argv[1]
+    overscan_jh_task(det_name)
---
+++ harnessed_jobs/overscan_BOT/v0/producer_overscan_BOT.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env ipython
+"""
+Producer script for BOT OVERSCAN analysis.
+"""
+import os
+from overscan_jh_task import overscan_jh_task
+from bot_eo_analyses import get_analysis_types, run_python_task_or_cl_script
+
+if 'overscan' in get_analysis_types():
+    overscan_task_script \
+        = os.path.join(os.environ['EOANALYSISJOBSDIR'], 'harnessed_jobs',
+                       'overscan_BOT', 'v0', 'overscan_jh_task.py')
+
+    run_python_task_or_cl_script(overscan_jh_task, overscan_task_script)
