6ad35b47e8674fc9fdc5814e827c6910b69d436a
James Chiang <jchiang@slac.stanford.edu>
2020-03-09 10:55:32 -0700

    fix docstrings and use glob pattern for overscan task

--- harnessed_jobs/overscan_BOT/v0/overscan_jh_task.py
+++ harnessed_jobs/overscan_BOT/v0/overscan_jh_task.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env ipython
 """
-Producer script for BOT overscan analysis.
+Harnessed job script for BOT overscan analysis.
 """
 def overscan_jh_task(det_name):
     """JH version of single sensor execution of the overscan task."""
@@ -12,10 +12,11 @@ def overscan_jh_task(det_name):
     file_prefix = make_file_prefix(run, det_name)
     acq_jobname = siteUtils.getProcessName('BOT_acq')
 
-    flat_files = siteUtils.dependency_glob(glob_pattern('ptc', det_name),
+    flat_files = siteUtils.dependency_glob(glob_pattern('overscan', det_name),
                                            acq_jobname=acq_jobname)
     if not flat_files:
-        print("ptc_task: Flat pairs files not found for detector", det_name)
+        print("overscan_task: Flat pairs files not found for detector",
+              det_name)
         return None
 
     eotest_results_file = '{}_eotest_results.fits'.format(file_prefix)
