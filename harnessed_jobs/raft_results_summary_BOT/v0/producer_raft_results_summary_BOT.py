6c42de01a1f93443b0a818930c3be13d22a54d02
James Chiang <jchiang@slac.stanford.edu>
2020-09-10 22:44:23 -0700

    use correct schema name for tearing results

--- harnessed_jobs/raft_results_summary_BOT/v0/producer_raft_results_summary_BOT.py
+++ harnessed_jobs/raft_results_summary_BOT/v0/producer_raft_results_summary_BOT.py
@@ -51,8 +51,8 @@ def make_focal_plane_plots():
                   'bright_fatter_BOT': (('bf_xcorr', 'bf_ycorr'), (None, None),
                                         (None, None), (False, False),
                                         ('1', '1')),
-                  'tearing_BOT': (('tearing_detections',), (None,), (None,),
-                                  (False,), ('1',))}
+                  'tearing_stats_BOT': (('tearing_detections',), (None,), (None,),
+                                        (False,), ('1',))}
 
     camera = camera_info.camera_object
 
