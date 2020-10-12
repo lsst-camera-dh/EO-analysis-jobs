19596af396209fb451ace6024080890f1ae9408e
James Chiang <jchiang@slac.stanford.edu>
2020-10-12 16:40:05 -0700

    switch to 2 batches for >100 devices for job dispatch and enable override via lcatr.cfg

--- python/ssh_dispatcher.py
+++ python/ssh_dispatcher.py
@@ -218,7 +218,7 @@ class TaskRunner:
 
 def ssh_device_analysis_pool(task_script, device_names, cwd='.', setup=None,
                              max_time=None, remote_hosts=None, verbose=False,
-                             max_retries=1, njobs=8):
+                             max_retries=1):
     """
     Submit JH tasks on remote nodes.
 
@@ -246,8 +246,6 @@ def ssh_device_analysis_pool(task_script, device_names, cwd='.', setup=None,
         Flag for additional diagnostic output.
     max_retries: int [1]
         Maximum number of retries for failed tasks.
-    njobs: int [8]
-        Maximum number of jobs to run on each of the lsst-dc nodes.
 
     Raises
     ------
@@ -261,13 +259,16 @@ def ssh_device_analysis_pool(task_script, device_names, cwd='.', setup=None,
     task_runner = TaskRunner(task_script, cwd, setup, max_retries=max_retries,
                              remote_hosts=remote_hosts, verbose=verbose)
     # In order to limit memory usage and to avoid overloading the file
-    # server, divide into batches so that no more than njobs are
-    # running on a single node.
+    # server, divide into 2 batches by default if processing for more
+    # than 100 devices is requested.
     ndev = len(device_names)
-    num_hosts = task_runner.remote_hosts.num_hosts
-    #num_batches = ndev//(njobs*num_hosts) + 1
     num_batches = 2 if ndev > 100 else 1
-    print(ndev, num_hosts, num_batches)
+
+    # Use override value from LCATR_NUM_BATCHES if it is set.
+    num_batches = os.environ.get('LCATR_NUM_BATCHES', num_batches)
+    print("# devices, # batches, # hosts:",
+          ndev, num_batches, task_runner.remote_hosts.num_hosts)
+
     bounds = np.linspace(0, ndev, num_batches + 1, dtype=int)
     print(bounds)
     for imin, imax in zip(bounds[:-1], bounds[1:]):
