77c2195fbbe66874d3cdb8be7d7fec71b2d5d5a0
James Chiang <jchiang@slac.stanford.edu>
2021-03-22 15:24:44 -0700

    add a standalone function to compute pd filter corrections

--- python/pd_filter_corrections.py
+++ python/pd_filter_corrections.py
@@ -11,6 +11,55 @@ from astropy.io import fits
 import pandas as pd
 
 
+def pd_filter_corrections(flux, Ne, filters, Ne_max=3e4):
+    """
+    Function to compute filter-specific corrections to the photodiode
+    integrals, bootstrapping from the data itself.
+
+    Parameters
+    ----------
+    flux: np.array
+        Photodiode integral values for each exposure in the sequence.
+    Ne: np.array
+        Measured signal level from the CCDs.  This will nominally be the
+        e-/pixel level for an amp in the CCD being analyzed.
+    filters: np.array
+        Array of filter combinations, e.g., 'empty_SDSSi', 'ND_OD0.1_SDSSi'.
+    Ne_max: float [3e4]
+        Maximum signal level (e-/pixel) to consider for fitting linear
+        scale factors for each filter combo. This should avoid low enough
+        to avoid rollover from B/F or nonlinearity at high signal levels.
+
+    Returns
+    -------
+    (np.array, dict)  The first element is a np.array of the correction
+    factors to apply directly to the flux array, and second element is a
+    dictionary of correction factors, keyed by filter combination.
+    """
+    # Determine filter order by sorting on signal level.
+    filter_list = []
+    for filt in filters[np.argsort(Ne)]:
+        if filt not in filter_list:
+            filter_list.append(filt)
+
+    # Fit the linear scale factor, y0, assuming the model Ne = y0*flux,
+    # independently for each filter under the requirement Ne < Ne_max.
+    y0 = dict()
+    for filt in filter_list:
+        index = np.where((filters == filt) & (Ne < Ne_max))
+        y0[filt] = sum(Ne[index])/sum(flux[index])
+
+    # Compute the corrections relative to the filter combination of the
+    # highest signal data.
+    pd_corrections = {filt: y0[filt]/y0[filter_list[-1]]
+                      for filt in filter_list}
+
+    # Return a numpy array of the correction factors and a dictionary
+    # of those corrections, keyed by filter combination.
+    return (np.array([pd_corrections[filt] for filt in filters]),
+            pd_corrections)
+
+
 def flat_metadata(flat_dir):
     """
     Extract frame metadata from the symlink name and physical path of
@@ -68,28 +117,18 @@ def apply_filter_corrections(detresp_file, data_dir, amp=1, max_signal=3e4):
                 for col in hdus[1].data.columns}
         df0 = pd.DataFrame(data=data)
 
-    # Add the filter info
+    # Add the filter info.
     df0['filt'] = [filters.get((dayobs, seqnum), 'None')
                    for dayobs, seqnum in zip(df0['DAYOBS'], df0['SEQNUM'])]
     df = pd.DataFrame(df0.query(f'filt != "None" and {ycol} < {max_signal}'))
 
-    # Sort filters by flux, in ascending order.
-    filters = sorted(list(set(df['filt'])),
-                     key=lambda x: np.median(df.query(f'filt=="{x}"')[xcol]))
-
-    # Fit the linear scale factor independently for the data associated
-    # with each filter combo.
-    y0 = dict()
-    for filt in filters:
-        my_df = df.query(f'filt=="{filt}"')
-        y0[filt] = sum(my_df[ycol])/sum(my_df[xcol])
-
-    # Compute corrections relative to the filter combination of the
-    # highest signal data.
-    pd_corrections = {filt: y0[filt]/y0[filters[-1]] for filt in filters}
+    # Compute the correction factors.
+    corr_factors, pd_corrections \
+        = pd_filter_corrections(df[xcol].to_numpy(), df[ycol].to_numpy(),
+                                df['filt'].to_numpy())
 
     # Apply the corrections.
-    df[xcol] *= np.array([pd_corrections.get(filt, 1) for filt in df['filt']])
+    df[xcol] *= corr_factors
 
     # Return a tuple of the corrected detector response data and the
     # corrections for each filter combination.
