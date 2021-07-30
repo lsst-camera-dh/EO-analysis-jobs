e4ea0bfa4aa617790d976298b1bd95575b818f3f
James Chiang <jchiang@slac.stanford.edu>
2021-07-30 11:12:58 -0700

    revert change to test for dark_current_fit

--- harnessed_jobs/dark_current_BOT/v0/producer_dark_current_BOT.py
+++ harnessed_jobs/dark_current_BOT/v0/producer_dark_current_BOT.py
@@ -6,8 +6,7 @@ import os
 from dark_current_jh_task import dark_current_jh_task
 from bot_eo_analyses import get_analysis_types, run_python_task_or_cl_script
 
-analysis_types = get_analysis_types()
-if 'dark' in analysis_types or 'dark_current_fit' in analysis_types:
+if 'dark' in get_analysis_types():
     dark_current_script \
         = os.path.join(os.environ['EOANALYSISJOBSDIR'], 'harnessed_jobs',
                        'dark_current_BOT', 'v0', 'dark_current_jh_task.py')
